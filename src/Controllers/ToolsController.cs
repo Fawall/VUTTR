e684e7d409e6e7fa2888d63f81b9258a305aae61
fawall <fabiolopes7844@gmail.com>
2022-07-09 13:54:49 -0300

    Created ToolsController with post method

---
+++ src/Controllers/ToolsController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using src.Context;
+using src.Models;
+using src.ViewModel;
+
+namespace src.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ToolsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ToolsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Tags(Tools tools)
+        {
+
+            await _context.Tools.AddAsync(tools);
+            await _context.SaveChangesAsync();
+
+            ResultViewModel resultVM = new ResultViewModel{
+                Id = tools.ToolsId,
+                Title = tools.Title,
+                Link = tools.Link,
+                Tags = tools.Tags
+            };
+
+            return Ok(resultVM);
+
+
+        }
+    }
+}
