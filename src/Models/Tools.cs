fc7db7582138c64fd04a0dc701d4ee3fa39e75e6
fawall <fabiolopes7844@gmail.com>
2022-07-09 13:43:55 -0300

    Added JsonIgnore for hide property id

--- src/Models/Tools.cs
+++ src/Models/Tools.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 namespace src.Models
 {
     public class Tools
     {
         [Key]
+        [JsonIgnore]
         public int ToolsId {get; set;}
 
         [Required(ErrorMessage = "This Field can't null ")]
         [StringLength(32, ErrorMessage = "Limit this field exceeded: Limit is 32")]
-        public string? Title {get; set;}
+        public string Title {get; set;}
         public string? Link { get; set; }      
 
         public string[]? Tags {get; set;}
